8487eb6765b94095354b51e07cb2151f83d4be3e
Christian Lisangola <jochri3@gmail.com>
2023-12-11 13:06:15 +0300

    Point 3 : Affichage avec concatenation

---
+++ src/Point3Affichage.java
@@ -0,0 +1,10 @@
+public class Point3Affichage {
+    public static void main(String[] args) {
+        String firstName="Christian";
+        String lastName="Lisangola";
+        int age=17;
+        double weight=35.4;
+        // Je m'appelle Christian Lisangola, j'ai 17 ans, et je pèse 35.4 Kg.
+        System.out.println("Je m'appelle "+firstName+" "+lastName+", j'ai "+age+" ans, je pèse "+weight+"Kg");
+    }
+}
