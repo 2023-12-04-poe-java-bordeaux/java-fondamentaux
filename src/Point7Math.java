e25a82b367cd8e9a9fd7d3e61770ad1801dd4799
Christian Lisangola <jochri3@gmail.com>
2023-12-11 16:35:57 +0300

    Point 6 : next() function depth explainations

---
+++ src/Point7Math.java
@@ -0,0 +1,14 @@
+public class Point7Math {
+    public static void main(String[] args) {
+        final var PI=3.14;
+        final double g=9.8;
+
+        System.out.println(PI);
+        System.out.println(Math.PI);
+        System.out.println(g);
+
+        var nombre=9;
+        System.out.println(Math.sqrt(nombre));
+        System.out.println(Math.pow(nombre,2));
+    }
+}
