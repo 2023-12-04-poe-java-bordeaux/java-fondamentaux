1d2d0d30804814ec51e76115c171c5239850ffb4
Christian Lisangola <jochri3@gmail.com>
2023-12-11 13:21:57 +0300

    Point 4 : String inetrpolation

--- src/Point3Affichage.java
+++ src/Point3Affichage.java
@@ -3,8 +3,10 @@ public class Point3Affichage {
         String firstName="Christian";
         String lastName="Lisangola";
         int age=17;
-        double weight=35.4;
+        double weight=35.45;
         // Je m'appelle Christian Lisangola, j'ai 17 ans, et je pèse 35.4 Kg.
-        System.out.println("Je m'appelle "+firstName+" "+lastName+", j'ai "+age+" ans, je pèse "+weight+"Kg");
+        System.out.println("Je m'appelle "+firstName+" "+lastName+", j'ai "+age+" ans, et je pèse "+weight+"Kg.");
+        System.out.printf("Je m'appelle %s %s, j'ai %d ans, et je pèse %f Kg.\n",firstName,lastName,age,weight);
+        System.out.printf("Je m'appelle %s %s, j'ai %d ans, et je pèse %.1f Kg.",firstName,lastName,age,weight);
     }
 }
