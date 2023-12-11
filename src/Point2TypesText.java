9c565d1260d5d37e3cbfb2b5c85d470ce6e2e406
Christian Lisangola <jochri3@gmail.com>
2023-12-11 12:34:13 +0300

    Point 2 : Textes en Java

---
+++ src/Point2TypesText.java
@@ -0,0 +1,12 @@
+public class Point2TypesText {
+    public static void main(String[] args) {
+        String firstName="Christian";
+        String lastName="Lisangola";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        char gender='M';
+        System.out.println(gender);
+    }
+}
