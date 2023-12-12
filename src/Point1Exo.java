ea8ce4ea757cb150c0774105658a0a3c6dfd5663
Christian Lisangola <jochri3@gmail.com>
2023-12-12 13:53:51 +0300

    Point 9 Exo

---
+++ src/Point1Exo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Point1Exo {
+    public static void main(String[] args) {
+        var input=new Scanner(System.in);
+        System.out.print("Entrez un nombre : ");
+        var nombre=input.nextInt();
+
+        for(int c=1;c<=13;c++){
+            var produit=nombre*c;
+            System.out.printf("%d x %d = %d\n",nombre,c,produit);
+        }
+    }
+}
